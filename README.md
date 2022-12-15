baee8318f36b84f610faeadabe633f8c8ff41356
thet naing <thet5284@gmail.com>
2022-12-15 22:23:27 +0630

    0.0.1

---
+++ README.md
@@ -0,0 +1,1 @@
+#Remote Testtop Sofware UI
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'sans': ["Euclid-Circular-A"]
+      }
+    },
+  },
+  plugins: [],
+}
